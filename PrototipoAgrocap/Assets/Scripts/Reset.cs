9980b38017aaa513b86e77d9e67776ebd35d5dab
Juliadamassio <juliapereiradamasio@gmail.com>
2026-04-10 11:40:26 -0300

    Botão reset

--- PrototipoAgrocap/Assets/Scripts/Reset.cs
+++ PrototipoAgrocap/Assets/Scripts/Reset.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;  //Nescessario para gerenciar as cenas
 
-public class Rese : MonoBehaviour
+public class ReseT : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,6 +12,10 @@ public class Rese : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //Recarrega a cena atual
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
