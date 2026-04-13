3122b92be764c84ba8fb4c97f20fdc40c516324c
Flavinotfound <flaviolimaa301@gmail.com>
2026-04-13 09:09:08 -0300

    Bug de Materiais
    materiais bugados resolvidos e reorganização de arquivos

--- PrototipoAgrocap/Assets/Scripts/Plunger.cs
+++ PrototipoAgrocap/Assets/Scripts/Plunger.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class Plungler : MonoBehaviour
 {
+    // força maxima da bolinha / medidor de força        ( O PLUNGER SÓ FUNCIONA COM O POWERSLIDER Q É O MEDIDOR DE FORÇA )
     float power;
     float minPower = 0f;
     public float maxPower = 100f;
@@ -32,11 +33,13 @@ public class Plungler : MonoBehaviour
             ballReady = true;
             if (Input.GetKey(KeyCode.Space))
             {
+                //velocidade do medidor da barrinha encher (é proporcional a força tambem)
                 if (power <= maxPower)
                 {
                     power += 50 * Time.deltaTime;
                 }
             }
+            // força da bolinha ao pressionar barra de espaço, direção de impulso com base de onde ta apontado
             if (Input.GetKeyUp(KeyCode.Space))
             {
                 foreach (Rigidbody r in balllist)
