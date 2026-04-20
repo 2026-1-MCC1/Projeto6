59493ce3405410e7f8f5aedcf71ed1c0c5469502
Julia Damásio <juliapereiradamasio@gmail.com>
2026-04-19 22:52:37 -0300

    Ajustando o Menu.

--- PrototipoAgrocap/Assets/Scripts/MenuController.cs
+++ PrototipoAgrocap/Assets/Scripts/MenuController.cs
@@ -1,12 +1,8 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Video;
 
 public class MenuController : MonoBehaviour
 {
-    public VideoPlayer VideoPlayer;
-    public GameObject MenuOpcoes;
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -15,17 +11,6 @@ public class MenuController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!VideoPlayer.isPlaying && Input.anyKeyDown)     //Verifica se o videoplayer esta rolando e para clicar em qualquer tecla
-        {
-          VideoPlayer.Stop();
-            VideoPlayer.gameObject.SetActive(false);
-
-            // Ativa o menu de opções
-            if (MenuOpcoes != null)
-            {
-                MenuOpcoes.SetActive(true);
-            }
-
-        }
+        
     }
 }
