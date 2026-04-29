77b1befa40d808b1e644835fdd771c510fc28734
Julia Damásio <juliapereiradamasio@gmail.com>
2026-04-29 12:03:25 -0300

    Codigo Luz

--- PrototipoAgrocap/Assets/Scripts/Inventory.cs
+++ PrototipoAgrocap/Assets/Scripts/Inventory.cs
@@ -13,6 +13,9 @@ public class Inventory : MonoBehaviour
     public int Leite = 0;
     public int Chocolate = 0;
     public int Morango = 0;
+    public GameObject[] Luzes;
+    private GameObject LuzAtual;
+
 
     [Header("UI")]
 
@@ -22,28 +25,50 @@ public class Inventory : MonoBehaviour
     // Adiciona um ingrediente ao inventário
     public void AdicionarIngrediente(IngredienteTipo tipo)
     {
+        int indiceLuz = -1; // -1 pq  é uma convençao de programação  
+        // se fosse "0" no lugar de -1 o programa iria entender que 
+        // a luz que precisa acender é a 0 (a representação do -1 é vazio ou invalido)
+
         // Verifica qual ingrediente foi coletado
         switch (tipo)
         {
             case IngredienteTipo.Trigo:
                 Trigo++;
+                indiceLuz = 0;
                 break;
 
             case IngredienteTipo.Ovo:
                 Ovo++;
+                indiceLuz = 1;
                 break;
 
             case IngredienteTipo.Leite:
                 Leite++;
+                indiceLuz = 2;
                 break;
 
             case IngredienteTipo.Chocolate:
                 Chocolate++;
+                indiceLuz = 3;
                 break;
 
             case IngredienteTipo.Morango:
                 Morango++;
+                indiceLuz = 4;
                 break;
+        }
+
+        //Parte para acender e apagar as luzes da mesa conforme a coleta de itens
+
+ 
+        //Apaga a luz anterior caso tenha uma acessa
+        if (LuzAtual != null) LuzAtual.SetActive(false);
+        
+        //Acende uma nova luz
+        if (indiceLuz != -1 && indiceLuz < Luzes.Length)
+        {
+            Luzes[indiceLuz].SetActive(true);
+            LuzAtual = Luzes[indiceLuz];
         }
 
         // Exibe no console (debug)
@@ -63,6 +88,8 @@ public class Inventory : MonoBehaviour
         else
         {
             Debug.LogWarning("Texto do último item não está conectado!");
+            //Criar uma função para cada item depois para deixar organizado
+            //Arreumar o range
         }
     }
 }
